76042e4388136330064d942392fb5ef4d0527206
Bradley Bridges <bradley@bbridges.io>
2021-12-05 22:39:46 -0800

    Add day 6 solution

---
+++ 2021/06/lanternfish.clj
@@ -0,0 +1,23 @@
+(ns advent-2021.lanternfish
+  (:require [clojure.string :as str]))
+
+(defn simulate-fish
+  [input]
+  (let [init (reduce
+              (fn [fish days] (update fish days inc))
+              (vec (replicate 9 0))
+              input)
+        total-days 80]
+    (->> (range total-days)
+         (reduce
+          (fn [fish _] (-> (into (subvec fish 1) (subvec fish 0 1))
+                           (update 6 (partial + (first fish)))))
+          init)
+         (reduce + 0))))
+
+(defn read-input
+  [stream]
+  (let [raw (str/trim (slurp stream))]
+    (map #(Integer/parseInt %) (str/split raw #","))))
+
+(-> *in* read-input simulate-fish println)
